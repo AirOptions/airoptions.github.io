9efac0c663d4e74873753bbbdb87a249331f1120
pranavmalikk <pranavbball@gmail.com>
2020-12-27 13:07:11 -0500

    Create a React app and publish it to GitHub Pages

--- src/components/pages/Callouts/index.js
+++ src/components/pages/Callouts/index.js
@@ -8,20 +8,14 @@ export const Callouts = () => (
   <div className='callouts__section'>
     <h1 className='callouts__heading'>crawlerr</h1>
     <div className="callouts__container">
-      <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
-      <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
-      <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
+      <Tweet tweetId="1339944656138883072" options={{align: 'left', width: '400'}}/>
+      <Tweet tweetId="1334912438001692675" options={{align: 'left', width: '400'}}/>
+      <Tweet tweetId="1337108867948408843" options={{align: 'left', width: '400'}}/>
     </div>
     <div className="callouts__container">
-    <div className="callouts__container">
-      <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
-    </div>
-    <div className="callouts__container">
-      <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
-    </div>
-    <div className="callouts__container">
-      <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
-    </div>
+      <Tweet tweetId="1341050886915813377" options={{align: 'left', width: '400'}}/>
+      <Tweet tweetId="1334513945336565761" options={{align: 'left', width: '400'}}/>
+      <Tweet tweetId="1339964394030161923" options={{align: 'left', width: '400'}}/>
     </div>
   </div>
   </>
