44b1ad33cfdf6d87684bcfd0af22198d6c1c3d6f
pranavmalikk <pranavbball@gmail.com>
2020-12-27 02:56:37 -0500

    Create a React app and publish it to GitHub Pages

--- src/components/pages/Callouts/index.js
+++ src/components/pages/Callouts/index.js
@@ -6,7 +6,7 @@ import { Tweet } from 'react-twitter-widgets'
 export const Callouts = () => (
     <>
   <div className='callouts__section'>
-    <h1 className='callouts__heading'>Crawlouts</h1>
+    <h1 className='callouts__heading'>crawlerr</h1>
     <div className="callouts__container">
       <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
       <Tweet tweetId="841418541026877441" options={{height: '400px'}}/>
