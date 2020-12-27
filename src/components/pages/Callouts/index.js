607725a253dd88055f8c077e8cc370972b8ca801
pranavmalikk <pranavbball@gmail.com>
2020-12-27 00:53:59 -0500

    Create a React app and publish it to GitHub Pages

--- src/components/pages/Callouts/index.js
+++ src/components/pages/Callouts/index.js
@@ -8,7 +8,7 @@ import "./Callouts.css";
 export const Callouts = () => (
     <>
     <div className='callouts__section'>
-    <h1 className='callouts__heading'>CallNOUTouts</h1>
+    <h1 className='callouts__heading'>CallLOUDouts</h1>
     <div className="callouts__container">
     <div className = 'firstTweet'>
     <blockquote class="twitter-tweet" data-theme="light"><p lang="en" dir="ltr">🚨 These <a href="https://twitter.com/search?q=%24DQ&amp;src=ctag&amp;ref_src=twsrc%5Etfw">$DQ</a> are now at the 10.2 from .72 for 1317% BOOOOOM 🚨 <a href="https://t.co/MREgR0ncDv">https://t.co/MREgR0ncDv</a></p>&mdash; Air Options (@air_options) <a href="https://twitter.com/air_options/status/1339944656138883072?ref_src=twsrc%5Etfw">December 18, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
