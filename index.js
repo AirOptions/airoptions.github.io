7ce09bee70a9ecf1b81c437f4532af25ad352327
AirOptions <43391650+AirOptions@users.noreply.github.com>
2020-12-30 18:01:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import {BrowserRouter} from "react-router-dom"
 import {Router} from 'react-router'
 import App from "./src/App";
 
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-8PZT7GXKPD');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
 <App />, document.getElementById("root")
 );
